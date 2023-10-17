e6a8ec9beb9cb04e8fcacdd95f79ea3712e45020
Seokyung <xuejd17@gmail.com>
2023-10-17 23:25:13 +0900

    SCSS Live Server 설치 및 환경설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import "./styles/css/App.css";
+
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<h1>
+				Learning SCSS(SASS)
+				<span className="sub">지금은 오후 10시 55분!</span>
+			</h1>
+		</div>
+	);
 }
 
 export default App;
