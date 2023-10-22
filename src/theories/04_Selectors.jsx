6d899119738bef57de2f9ec1777f0c54e6b1bb06
Seokyung <xuejd17@gmail.com>
2023-10-22 14:27:49 +0900

    4강 버튼 클래스명 수정

--- src/theories/04_Selectors.jsx
+++ src/theories/04_Selectors.jsx
@@ -5,8 +5,8 @@ function Selectors() {
 		<>
 			<h2>04 대표적인 CSS 선택자 SCSS에서 만들기</h2>
 			<div className="selectors">
-				<button className="btn confirm">Confirm</button>
-				<button className="btn warning">Warning</button>
+				<button className="selectors-btn confirm">Confirm</button>
+				<button className="selectors-btn warning">Warning</button>
 				{/* <a>태그 선택자</a>
 				<a className="selectors-red">클래스 선택자</a>
 				<a id="selectors-red">아이디 선택자</a>
