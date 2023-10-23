66c15bf8416de4c44c997c4bba42253a14ba1bf2
Seokyung <xuejd17@gmail.com>
2023-10-23 17:59:42 +0900

    10 변수(Variables) #2

--- src/theories/10_Variables.jsx
+++ src/theories/10_Variables.jsx
@@ -2,15 +2,10 @@ import "../styles/theories/10_Variables.scss";
 
 export default function Variables() {
 	return (
-		<div className="variables">
-			<div>
-				<h3>Variables Lesson #1</h3>
-				<div className="variables-btn-container">
-					<button className="variables-btn primary">Primary Button</button>
-					<button className="variables-btn secondary">Secondary Button</button>
-					<button className="variables-btn warning">Warning Button</button>
-				</div>
-			</div>
+		<div className="variables-btn-container">
+			<button className="variables-btn primary">Primary Button</button>
+			<button className="variables-btn secondary">Secondary Button</button>
+			<button className="variables-btn warning">Warning Button</button>
 		</div>
 	);
 }
