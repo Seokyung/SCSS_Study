0fd6c38d59878b0af17c60a2d1c3266dfee58716
Seokyung <xuejd17@gmail.com>
2023-11-09 11:55:44 +0900

    예제 #02 @import로 reset.css와 normalize.css 연결하기

---
+++ src/examples/02_ResetNormalize.jsx
@@ -0,0 +1,32 @@
+import "../styles/examples/02_ResetNormalize.scss";
+
+function ResetNormalize() {
+	return (
+		<div className="reset-normalize-container">
+			<section>
+				<h1>Reset CSS</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem placeat
+					officiis similique ipsum? Voluptatum commodi fugiat iusto. Cumque
+					repellendus repellat, minus sit, id autem dolore vero incidunt
+					molestiae dicta aliquam.
+				</p>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+					dignissimos nihil voluptas corporis eligendi. Ab ad earum magnam
+					reprehenderit placeat, doloremque ut quod similique, eius quae dolores
+					saepe? Vero, a ab soluta magnam velit, commodi perspiciatis est facere
+					quas eum sapiente alias libero adipisci ex.
+				</p>
+				<ul>
+					<li>List #01</li>
+					<li>List #02</li>
+					<li>List #03</li>
+					<li>List #04</li>
+				</ul>
+			</section>
+		</div>
+	);
+}
+
+export default ResetNormalize;
--- src/routes/ExampleRouter.jsx
+++ src/routes/ExampleRouter.jsx
@@ -1,11 +1,17 @@
 import { useNavigate, useParams } from "react-router-dom";
 import SelectorVirtualClass from "../examples/01_SelectorVirtualClass";
+import ResetNormalize from "../examples/02_ResetNormalize";
 
 export const exampleList = [
 	{
 		id: 1,
 		title: "01. 주요 CSS 선택자, 주요 가상클래스 활용하기",
 		component: <SelectorVirtualClass />,
+	},
+	{
+		id: 2,
+		title: "02. @import로 reset.css와 normalize.css 연결하기",
+		component: <ResetNormalize />,
 	},
 ];
 
