db10b3d596c4b52b1765213e95bb0367cc070426
Seokyung <xuejd17@gmail.com>
2023-11-14 17:20:56 +0900

    실전예제 #01 가상클래스를 활용한 살아있는 애니메이션 하트

---
+++ src/exercises/01_AnimatedHeart.jsx
@@ -0,0 +1,11 @@
+import "../styles/exercises/01_AnimatedHeart.scss";
+
+function AnimatedHeart() {
+	return (
+		<div className="animated-heart-container">
+			<div className="heart"></div>
+		</div>
+	);
+}
+
+export default AnimatedHeart;
