2f6a62da0a43890d131b8557c8befe2596df7203
Seokyung <xuejd17@gmail.com>
2023-11-15 19:23:35 +0900

    심장 박동 속도 조절 버튼 추가

--- src/exercises/01_AnimatedHeart.jsx
+++ src/exercises/01_AnimatedHeart.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import "../styles/exercises/01_AnimatedHeart.scss";
 
 function AnimatedHeart() {
+	const [isChanged, setIsChanged] = useState(false);
+	// const heartBeat = getComputedStyle(
+	// 	document.querySelector(":root")
+	// ).getPropertyValue("--beating-speed");
+
+	const onBtnClick = () => {
+		if (isChanged) {
+			document
+				.querySelector(":root")
+				.style.setProperty("--beating-speed", "1s");
+		} else {
+			document
+				.querySelector(":root")
+				.style.setProperty("--beating-speed", "2s");
+		}
+		setIsChanged((prev) => !prev);
+	};
+
 	return (
 		<div className="animated-heart-container">
 			<div className="heart"></div>
+			<button onClick={onBtnClick}>Change</button>
 		</div>
 	);
 }
